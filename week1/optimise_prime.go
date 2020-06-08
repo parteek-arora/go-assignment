a5e91e5dbcea7ef57e605ae2335f053cb8023573
parteekArora <parteek.debutinfotech.com>
2020-06-08 09:52:57 +0530

    optimise prime number

---
+++ week1/optimise_prime.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+import "math"
+
+var number, count int = 2, 0
+
+func main() {
+	var i, n int
+	fmt.Print("Enter the  Number : ")
+	fmt.Scanln(&n)
+	fmt.Println("The number entered is : ", n)
+	fmt.Println("The prime numbers are :- ")
+	
+	// get the prime number
+	for count <= n-1 {
+		prime := true;
+		for i = 2; i <= int(math.Sqrt(float64(number))); i++ {
+			if number%i == 0 {
+				prime = false;
+				break
+			}
+		}
+		if prime == true {
+			count++
+			fmt.Println(number)
+		}
+		number++
+	}
+}
